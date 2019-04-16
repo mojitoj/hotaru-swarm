7e5600a30e238fe4bf377bfeb32600f9a8754152
Mohammad Jafari <jafarim@gmail.com>
2019-04-16 12:17:54 -0700

    fixing config errors.

--- config/prod.exs
+++ config/prod.exs
@@ -14,7 +14,7 @@ config :hotaru_swarm, HotaruSwarmWeb.Endpoint,
   url: [scheme: "https", host: "hotaru-swarm", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
-  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
